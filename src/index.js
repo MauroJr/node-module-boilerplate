aad94833c78752f6bc87c77b258c665362b93a35
Mauro Junior <mauropereirajr@gmail.com>
2018-11-11 00:26:53 -0200

    feat(config): :sparkles: added \`node-config\`

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 'use strict';
 
-exports.hello = function hello() {
-  return process.env.HELLO;
+const config = require('config');
+
+
+exports.fromEnv = {
+  HELLO() {
+    return process.env.HELLO;
+  }
+};
+
+exports.fromConfig = {
+  hi() {
+    return config.get('hi');
+  },
+
+  hello() {
+    return config.get('hello');
+  }
 };
--- test/index.test.js
+++ test/index.test.js
@@ -1,14 +1,19 @@
 'use strict';
 
 const { expect } = require('chai');
-const { hello } = require('../src');
+const { fromConfig, fromEnv } = require('../src');
 
-describe('Importing The index module', function () {
-  it('should be a function', function () {
-    expect(hello).to.be.a('function');
+
+describe('Checking Environment Variable `HELLO`', function () {
+  it('should return value from environment variable', function () {
+    expect(fromEnv.HELLO()).to.be.equal('WORLD');
   });
 
-  it('should return `HELLO` string from env', function () {
-    expect(hello()).to.be.equal('HELLO');
+  it('should return value from config file', function () {
+    expect(fromConfig.hi()).to.be.equal('hi from config');
+  });
+
+  it('should return value from custom environment variable', function () {
+    expect(fromConfig.hello()).to.be.equal('WORLD');
   });
 });
